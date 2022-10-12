e1a91195d28d64f3509138506e031c6ac1845012
Tony Wang <tony94042@gmail.com>
2022-10-12 14:27:22 +1100

    Playing around with some scenarios

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  function printAge(){
+    const outPut = `You are ${firstName} and you are ${age}.`;
+    return outPut
+  }
+}
+const firstName = "Tony";
+const hello = calcAge(1989)
